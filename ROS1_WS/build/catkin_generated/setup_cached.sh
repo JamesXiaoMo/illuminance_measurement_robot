c0ceb465a6f1d95a354c736db4476cf934f22d1b
JamesXiaoMo <jameswuxiaomo@gmail.com>
2025-05-13 16:53:40 +0900

    add old codings

--- ROS1_WS/build/catkin_generated/setup_cached.sh
+++ ROS1_WS/build/catkin_generated/setup_cached.sh
@@ -9,6 +9,8 @@
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/illuminance_measurement_robot/ROS1_WS/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH="/home/illuminance_measurement_robot/ROS1_WS/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/illuminance_measurement_robot/ROS1_WS/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/illuminance_measurement_robot/ROS1_WS/build'
+export PYTHONPATH="/home/illuminance_measurement_robot/ROS1_WS/devel/lib/python3/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES='/home/illuminance_measurement_robot/ROS1_WS/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/illuminance_measurement_robot/ROS1_WS/src:$ROS_PACKAGE_PATH"
